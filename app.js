ce92d470d159d5fa13c87c7b716b64eac4fd3070
Erika Pauwels <erika.pauwels@gmail.com>
2016-08-28 09:08:21 +0200

    Add JSONAPI and SPARQL support

--- app.js
+++ app.js
@@ -5,27 +5,44 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+// apiai SDK
+var apiai = require('apiai');
 
+// validations on startup
+if (process.env.MU_SPARQL_ENDPOINT === undefined) {
+  throw new Error('Environment variable MU_SPARQL_ENDPOINT is missing');
+}
+if (process.env.MU_APPLICATION_GRAPH === undefined) {
+  throw new Error('Environment variable MU_APPLICATION_GRAPH is missing');
+}
 if (process.env.APIAI_ACCESS_TOKEN === undefined) {
   throw new Error('Environment variable APIAI_ACCESS_TOKEN is missing');
 }
 
+// create Express app
 var app = express();
 
 // set application-wide variables
-var apiai = require('apiai');
+app.locals.graph = process.env.MU_APPLICATION_GRAPH;
+app.locals.sessionIdHeader = 'MU_SESSION_ID';
+app.locals.rewriteUrlHeader = 'X_REWRITE_URL';
 app.locals.apiaiApp = apiai(process.env.APIAI_ACCESS_TOKEN);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app configuration
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json( { type: function(req) { return /^application\/vnd\.api\+json/.test(req.get('content-type')); } } ));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// routing
+var routes = require('./routes/index');
 app.use('/', routes);
+
+// set JSONAPI content type
+app.use('/', function(req, res, next) {
+  res.type('application/vnd.api+json');
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
